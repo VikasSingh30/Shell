5694cc427274f00527076bb7a36df6077db1f8a7
VikasSingh30 <singhvikas.sv30@gmail.com>
2025-02-11 12:25:52 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -30,21 +30,38 @@ const rl = readline.createInterface({
 // });
 
 //EXIT 0 implemented
+const builtins = new Set(["echo", "exit", "type"  ]); // line addes for type builtin to work with other built in
 function prompt() {
   process.stdout.write("$ "); // Display prompt without newline
 
   rl.once("line", (input) => {
     const trimmedInput = input.trim(); // line addes for echo builtin
-    // if (input.trim() === "exit 0")  // to implement exit exit 0 line was addes
+    // if (input.trim() === "exit 0")  // to implement exit exit 0 line was added
+    const args = trimmedInput.split(" "); // Split input into command and arguments (line addes for type builtin)
+    const command = args[0]; // Extract command from input (line addes for type builtin)
      if (trimmedInput === "exit 0") {   // line addes to implement echo
       rl.close();
       process.exit(0); // Exit with status code 0
     }
     //function for echo implementation
-    if (trimmedInput.startsWith("echo")) {
-      console.log(trimmedInput.slice(5)); // Print the argument to echo
-    }else{
-      console.log(`${trimmedInput}: command not found`); // Print the command not found
+    // if (trimmedInput.startsWith("echo")) {
+    //   console.log(trimmedInput.slice(5)); // Print the argument to echo
+    // }else{
+    //   console.log(`${trimmedInput}: command not found`); // Print the command not found
+    // }
+
+    // for type built in
+    if (command === "echo") {
+      console.log(args.slice(1).join(" ")); // Print everything after "echo"
+    } else if (command === "type") {
+      const checkCommand = args[1]; // The command to check
+      if (builtins.has(checkCommand)) {
+        console.log(`${checkCommand} is a shell builtin`);
+      } else {
+        console.log(`${checkCommand}: not found`);
+      }
+    } else {
+      console.log(`${trimmedInput}: command not found`);
     }
 
     // console.log(`${input}: command not found`); // exit 0 line
