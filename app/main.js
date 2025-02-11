c24664eb70bc6c6313ab6e9a2217d27504782c04
VikasSingh30 <singhvikas.sv30@gmail.com>
2025-02-11 12:10:43 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -4,30 +4,49 @@ const { REPLServer } = require("repl");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: "$ "
+  // prompt: "$ "  // prompt for repl
 });
 
-rl.prompt();    // initial prompt
+// rl.prompt();    // initial prompt for repl
 
 // rl.question("$ ", (answer) => {
 //   console.log(`${answer}: command not found`);
 //   rl.close();
 // });
-rl.on("line", (input) => {
-if (input.trim() === "exit") {
-  rl.close();
-  return;
-}
-try{
-  let result = eval(input);
-  console.log(result);
-}catch(e){
-  console.log(`${input}: command not found`);
-}
+//REPL implemented
+// rl.on("line", (input) => {
+// if (input.trim() === "exit") {
+//   rl.close();
+//   return;
+// }
+// try{
+//   let result = eval(input);
+//   console.log(result);
+// }catch(e){
+//   console.log(`${input}: command not found`);
+// }
  
-  rl.prompt();
-});
+//   rl.prompt();
+// });
+
+//EXIT 0 implemented
+function prompt() {
+  process.stdout.write("$ "); // Display prompt without newline
+
+  rl.once("line", (input) => {
+    if (input.trim() === "exit 0") {
+      rl.close();
+      process.exit(0); // Exit with status code 0
+    }
+
+    console.log(`${input}: command not found`);
+    prompt(); // Continue loop
+  });
+}
+
+prompt(); // Start REPL
+
 rl.on("close", () => {
   console.log("Exiting...");
-  // process.exit(0);
+  process.exit(0);
 });
